4c4dcbe71f90d60b30385a482a5ade10a43a3ed7
Julien3556 <julius.clairay@gmail.com>
2025-02-26 16:26:33 +0100

    Ajout de parse_data dans le main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Importation
 import pandas as pd
 import detect_scan_port as sp
+import parse_data as pa
 
 # Définition des noms de colonnes
 columns = [
@@ -15,10 +16,11 @@ dataFrame = pd.read_csv("conn_sample.log", sep="\t", names=columns, engine="pyth
 # Correction du format timestamp
 dataFrame['ts'] = pd.to_datetime(dataFrame['ts'].astype(float), unit='s')
 
+file = 'conn_sample.log'
 
 if __name__ == "__main__" :
     while True:
-        print("\n\Commandes possibles : afficher, scans de ports (sp), scans de ports2 (sp2)")
+        print("\n\Commandes possibles : \nselect_file (select), \nafficher, \nscans de ports (sp), \nscans de ports2 (sp2), \nconvert_data (convert)")
         commandes = input(">>> : ")
         
         if commandes == "quitter" or commandes == "q":
@@ -28,6 +30,9 @@ if __name__ == "__main__" :
         elif commandes == "afficher":
             print(dataFrame.head())
             
+        elif commandes == "select_file" or commandes == "select":
+            file = input("File name : ")
+            
         elif commandes == "scans de ports" or commandes == "sp":
             scan = sp.Scans(dataFrame)
             scan.scans()
@@ -35,6 +40,10 @@ if __name__ == "__main__" :
         elif commandes == "scans de ports2" or commandes == "sp2":
             scan2 = sp.Scans(dataFrame)
             scan2.scans2()
+            
+        elif commandes == "convert_data" or commandes == "convert":
+            data = pa.convert_data(file)
+            print(data.sample(20))
     
         else:
             print("Erreur de commande")
