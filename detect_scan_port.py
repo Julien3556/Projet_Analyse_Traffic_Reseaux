31b4660fb4378e1bb1ead685e0b0c39f0d00751e
Julien3556 <julius.clairay@gmail.com>
2025-03-07 10:28:19 +0100

    add

--- detect_scan_port.py
+++ detect_scan_port.py
@@ -1,13 +1,13 @@
 import pandas as pd
 def scans(dataFrame):
-    port_scan_attempts = dataFrame.groupby("id.orig_h")["id.resp_p"].nunique() #Tableau grand nb de connexions
+    port_scan_attempts = dataFrame.groupby("id.orig_h")["id.resp_p"].nunique() # Tableau qui comprend un grand nb de connexions
     suspected_scanners = port_scan_attempts[port_scan_attempts > 50]
 
-    rejected_connections = dataFrame[dataFrame["conn_state"] == "REJ"] #Tableau de connexions rejetés
+    rejected_connections = dataFrame[dataFrame["conn_state"] == "REJ"] # Tableau qui comprend les connexions rejetés
     connections_rejected = rejected_connections.groupby("id.orig_h").size()
 
 
-    fusion = port_scan_attempts.index.intersection(connections_rejected.index)
+    fusion = port_scan_attempts.index.intersection(connections_rejected.index) # Tableau qui fusionne les 2 tableaux précédents
     print(list(fusion))
 
     print("IPs suspectées de scan de ports: ")
