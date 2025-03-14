e6d080c78667ba8d5a193638362db0f90a67beec
MAHE Noah <noahmahe56@gmail.com>
2025-03-14 10:53:59 +0100

    finish ip_connexionTime from basic_stat.py

--- basic_stat.py
+++ basic_stat.py
@@ -1,45 +1,51 @@
-# Importation des bibliothèques nécessaires
+# Import necessary libraries
 import pandas as pd  
 import matplotlib.pyplot as plt  
-from parse_data import parse_log  # Importer la fonction parse_log
+from parse_data import parse_log  # Import the parse_log function
 
 def ip_nbPort(dataFrame):
-    # Comptage du nombre de ports distincts contactés par chaque adresse IP source (src)
+    # Count the number of distinct ports contacted by each source IP address (src)
     port_scan_attempts = dataFrame.groupby("src")["dst_port"].nunique()
 
-    # Filtrage : on ne garde que les IP ayant contacté plus de 50 ports distincts
+    # Filter: keep only IPs that contacted more than a specified number of distinct ports
     limit = int(input("Select the minimum number of ports : "))
     port_scan_attempts = port_scan_attempts[port_scan_attempts > limit]
     print(port_scan_attempts)
 
-    # Création du graphique
+    # Create the chart
     port_scan_attempts.plot(kind="bar", color="skyblue", edgecolor="black")
 
-    # Ajout des labels et du titre au graphique
-    plt.xlabel("Adresse IP source")  
-    plt.ylabel("Nombre de ports distincts contactés")  
-    plt.title("Nombre de tentatives de connexion par adresse IP")  
+    # Add labels and title to the chart
+    plt.xlabel("Source IP address")  
+    plt.ylabel("Number of distinct ports contacted")  
+    plt.title("Number of connection attempts per IP address")  
     plt.xticks(rotation=45, fontsize=6)  
     plt.grid(axis="y", linestyle="--", alpha=0.7)  
 
-    # Affichage du graphique
+    # Display the chart
     plt.show()
 
 
 def ip_connexionTime(dataFrame):
-    connexionTime = dataFrame.groupby("src")["duration"].mean()
-    limit = int(input("Select the minimum duration : "))
+    connexionTime = dataFrame.groupby("src")["duration"].max()
+
+    # Filter: keep only IPs that the average connection duration is greater than a specified value
+    limit = float(input("Select the minimum duration : "))
     connexionTime = connexionTime[connexionTime > limit]
+
+    # Create the chart
     connexionTime.plot(kind="bar", color="skyblue", edgecolor="black")
-    plt.xlabel("Adresse IP source")
-    plt.ylabel("Durée de connexion")
+
+    # Add labels and title to the chart
+    plt.xlabel("Source IP address")
+    plt.ylabel("Maximum connection duration")
+    plt.title("Connection duration per IP address")  
     plt.xticks(rotation=45, fontsize=6)  
     plt.grid(axis="y", linestyle="--", alpha=0.7)  
     plt.show()
 
 
-
 if __name__ == '__main__':
-    # Utiliser parse_log pour charger les données
+    # Use parse_log to load the data
     dataFrame = parse_log("data/conn_sample.log")
     ip_connexionTime(dataFrame)
