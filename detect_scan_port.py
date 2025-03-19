7056be4e021da2ad7e4e058366a67724029bfc24
Julien3556 <julius.clairay@gmail.com>
2025-03-19 15:59:21 +0100

    threshold

--- detect_scan_port.py
+++ detect_scan_port.py
@@ -4,8 +4,8 @@ from parse_data import parse_log
 
 def scans(dataFrame):
     port_scan_attempts = dataFrame.groupby("src")["dst_port"].nunique() # Tableau qui comprend un grand nb de connexions
-    limit = int(input("Select the minimum number of ports : "))
-    suspected_scanners = port_scan_attempts[port_scan_attempts > limit]
+    threshold = int(input("Select the threshold : "))
+    suspected_scanners = port_scan_attempts[port_scan_attempts > threshold]
 
     rejected_connections = dataFrame[dataFrame["conn_state"] == "REJ"] # Tableau qui comprend les connexions rejetées
     connections_rejected = rejected_connections.groupby("src").size()
